c62c2730e6edc45199a5c6f51a04c2eaf10b19fb
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-03-28 13:15:35 +0200

    Create product service

---
+++ src/app/services/product.service.spec.ts
@@ -0,0 +1,49 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
+import { ProductsService } from './product.service';
+import { Product } from '../models/product.model';
+import { generateManyProducts } from '../models/product.mock';
+import { environment } from './../../environments/environment';
+
+fdescribe('ProductsService', () => {
+    let productService: ProductsService;
+    let httpController: HttpTestingController;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [
+                ProductsService
+            ]
+        });
+        productService = TestBed.inject(ProductsService);
+        httpController = TestBed.inject(HttpTestingController);
+    });
+
+    it('should be create', () => {
+        expect(productService).toBeTruthy();
+    });
+
+    describe('tests for getAllSimple', () => {
+        it('should return a product list', (doneFn) => {
+            //Arrange
+            const mockData: Product[] = generateManyProducts(2);
+            //Act
+            productService.getAllSimple()
+                .subscribe((data) => {
+                    //Assert
+                    expect(data.length).toEqual(mockData.length);
+                    expect(data).toEqual(mockData);
+                    doneFn();
+                });
+
+            // http config
+            const url = `${environment.API_URL}/api/v1/products`;
+            const req = httpController.expectOne(url);
+            req.flush(mockData);
+            httpController.verify();
+        });
+    });
+
+});
