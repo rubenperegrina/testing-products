1ac08693355a4b934982fcec049d48c05f851bd8
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-04-03 15:55:07 +0200

    Update karma.conf

--- karma.conf.js
+++ karma.conf.js
@@ -34,10 +34,10 @@ module.exports = function (config) {
       ],
       check: {
         global: {
-          statements: 80,
-          branches: 80,
-          funtions: 80,
-          lines: 80
+          statements: 50,
+          branches: 50,
+          functions: 50,
+          lines: 50
         }
       }
     },
